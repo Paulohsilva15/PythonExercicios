0ab560b1633959616ddaace1172f5d573d4f9980
PauloHsilva15 <paulohenrique_brandao@live.com>
2026-05-29 15:09:07 -0300

    continuação e aprimoramento em curso em video

---
+++ frase.py
@@ -0,0 +1,10 @@
+frase = ('Curso em Video Python')
+print(frase[:5])
+
+
+# 3:13 quarta letra ate o 12
+# :13 do inicio ate a casa 12 o 13 nao conta por começar no 0,1,2,3 e
+# assim vai até o 13 que termina no 12.
+#[1:15:2]) seria do 1 ao 15 pulando em 2 -> us mvdo
+#print(frase.replace('Python', 'Android')) troca o python
+# por android :  curso em video android
