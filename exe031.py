d33d18838454d7fd625bf995b0aca4e68270dba1
PauloHsilva15 <paulohenrique_brandao@live.com>
2026-06-01 15:31:37 -0300

    Aprendendo a usar if e else:

---
+++ exe031.py
@@ -0,0 +1,6 @@
+numero = int(input('diga um numro qualquer:'))
+resultado = numero % 2
+if resultado == 0:
+    print('o numero {} é par'.format(numero))
+else:
+    print('o numero {} é impar'.format(numero))
