1674f434ba260f41f4982e2109724142ea18230e
PauloHsilva15 <paulohenrique_brandao@live.com>
2026-06-04 12:59:51 -0300

    Aprendendo a usar cores !!

---
+++ aula11.py
@@ -0,0 +1,3 @@
+print ('\033[4;30;45m Olá ,Mundo ! \033[m')
+nome = str(input('\033[7;30;45m Qual o seu nome ?\033[m'))
+print ('\033[4;30;45m Muito prazer!! \033[m \033[7:30:45m  {} \033[m !!!'.format(nome))
